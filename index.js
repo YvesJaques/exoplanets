6bfd726fa702c96f9b3d88890b03311c752ee652
Yves Jaques <ymoraisjaques@gmail.com>
2024-01-14 16:40:57 -0300

    Reading file data

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const parse = require('csv-parse');
+const { parse } = require('csv-parse');
+const fs = require('fs');
+
+const results = []
+
+fs.createReadStream('kepler_data.csv')
+    .on('data', (data) => {
+        results.push(data)
+    })
+    .on('error', (err) => {
+        console.log(err)    
+    })
+    .on('end', (err) => {
+        console.log(results)
+        console.log('done')
+    })
+// parse();
